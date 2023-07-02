45c58fe6c56e0cb8b777eabb8e44f4a35970d247
Paula Ochoa <paogutierrez9106@outlook.com>
2023-07-02 10:18:04 -0500

    Se crea la función Search y se le hace llamado desde el resultsHTML

--- js/script.js
+++ js/script.js
@@ -1,8 +1,51 @@
+var users = [
+    {
+        name: 'Pedro',
+        gender: 'M',
+        hobby: 'Pets',
+        avatar: 'avatar1.png'
+    },
+    {
+        name: 'Carlos',
+        gender: 'M',
+        hobby: 'Pets',
+        avatar: 'avatar2.png'
+    },
+    {
+        name: 'María',
+        gender: 'F',
+        hobby: 'Music',
+        avatar: 'avatar3.png'
+    },
+    {
+        name: 'Juliana',
+        gender: 'F',
+        hobby: 'Sports',
+        avatar: 'avatar4.png'
+    },
+    {
+        name: 'Lucía',
+        gender: 'F',
+        hobby: 'Reading',
+        avatar: 'avatar5.png'
+    },
+    {
+        name: 'Paul',
+        gender: 'M',
+        hobby: 'Shopping',
+        avatar: 'avatar6.png'
+    },
+    {
+        name: 'Sara',
+        gender: 'F',
+        hobby: 'Pets',
+        avatar: 'avatar7.png'
+    },
+];
+
 window.addEventListener('load', function(){
-    
-    var results = document.getElementById('results');
-    var searchBtn = document.getElementById('searchBtn');
-    searchBtn.addEventListener('click', function(){
+
+    function search(){
 
         //get hobby
         var hobbyField = document.getElementById('hobby');
@@ -13,8 +56,26 @@ window.addEventListener('load', function(){
         var genderField = document.getElementById('gender');
         var selected = genderField.selectedIndex;
         var gender = genderField.options[selected].value;
-        console.log(gender);
+        console.log(gender); 
 
-        results.innerHTML = 'Mostrar resultados';
-    });
+        var resultsHTML = '';
+        var numUsers = users.length;
+
+        for(var i = 0; i < numUsers; i++){
+            resultsHTML +=  '<div class="person-row">\
+                                <img src="images/' + users[i].avatar + '">\
+                                <div class="person-info">\
+                                    <div>' + users[i].name + '</div>\
+                                    <div>' + users[i].hobby + '</div>\
+                                </div>\
+                                <button>Add as friend</button>\
+                            </div>';
+        }
+
+        results.innerHTML = resultsHTML;
+    }
+    
+    var results = document.getElementById('results');
+    var searchBtn = document.getElementById('searchBtn');
+    searchBtn.addEventListener('click', search);
 });
