4c656340855d4663a2f4ab982a9f7a821a5c2e26
Paula Ochoa <paogutierrez9106@outlook.com>
2023-07-02 09:50:46 -0500

    Capturando los datos 'gender' y 'hobby' en scrip.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 window.addEventListener('load', function(){
-    console.log('se ha cargado la página');
-
+    
+    var results = document.getElementById('results');
     var searchBtn = document.getElementById('searchBtn');
     searchBtn.addEventListener('click', function(){
-        console.log('click');
+
+        //get hobby
+        var hobbyField = document.getElementById('hobby');
+        var hobby = hobbyField.value;
+        console.log(hobby);
+
+        //get gender
+        var genderField = document.getElementById('gender');
+        var selected = genderField.selectedIndex;
+        var gender = genderField.options[selected].value;
+        console.log(gender);
+
+        results.innerHTML = 'Mostrar resultados';
     });
 });
